d6ab46d20ded7ae4c9a458c200682479888ee445
Perry12 <47535069+Perry12th@users.noreply.github.com>
2021-02-07 14:30:34 -0500

    Completed Assignment 1

---
+++ Assets/Scripts/MinigameScript.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public enum MinigameMode { NotStarted, Scan, Extract, Finished }
+public class MinigameScript : MonoBehaviour
+{
+
+    [Header("UI")]
+    [SerializeField]
+    TextMeshProUGUI messages;
+    [SerializeField]
+    Image modeIcon;
+    [SerializeField]
+    TextMeshProUGUI modeText;
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+    [SerializeField]
+    Sprite extractSprite;
+    [SerializeField]
+    Sprite scanSprite;
+
+    [Header("Mini-game Info")]
+    [SerializeField]
+    int startScanAmount;
+    [SerializeField]
+    int startExtractAmount;
+    int score;
+    int scansLeft;
+    int extractsLeft;
+    bool isGameOver;
+    public MinigameMode mode = MinigameMode.NotStarted;
+    GameObject[] tiles = new GameObject[1024];
+    [SerializeField]
+    int NumberOfMaxTiles;
+    [SerializeField]
+    GameObject tileBase;
+
+    public void Start()
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            tiles[i] = Instantiate(tileBase);
+            tiles[i].transform.SetParent(transform);
+            int tileNum = (i + 1);
+            int rowNum = tileNum > 32 ? tileNum % 32 : tileNum;
+            int colNum = tileNum > 32 ? (tileNum / 32) : 0;
+            if (tileNum % 32 == 0)
+            {
+                rowNum = 32;
+                if (colNum != 0)
+                {
+                    colNum--;
+                }
+            }
+            TileScript tileScript = tiles[i].GetComponent<TileScript>();
+            tileScript.SetGridIndices(rowNum, colNum);
+        }
+
+        messages.text = "Welcome to this little tile base game. Objective is to scan titles to find the most valuable titles to get the highest score. Press the Start button to begin.";
+    }
+    public void ResetGame()
+    {
+        foreach(GameObject tile in tiles)
+        {
+            TileScript script = tile.GetComponent<TileScript>();
+            script.SetState(TileState.EMPTY);
+            script.SetVisability(false);
+        }
+        for (int i = 0; i < NumberOfMaxTiles; i ++)
+        {
+            int row = Random.Range(1, 32);
+            int col = Random.Range(0, 31);
+
+            SetTileMaterial(row, col);
+        }
+        scansLeft = startScanAmount;
+        extractsLeft = startExtractAmount;
+        score = 0;
+        scoreText.text = "Score: " + score;
+        mode = MinigameMode.Extract;
+        messages.text = "Extracts Left: " + extractsLeft;
+        modeText.text = "Extract Mode";
+        modeIcon.sprite = extractSprite;
+    }
+
+    public void SetTileMaterial(int row, int col)
+    {
+        for (int c = col - 2; c <= col + 2; c++)
+        {
+            for (int r = row - 2; r <= row + 2; r++)
+            {
+                if ((r >= 1 && r <= 32) && (c >= 0 && c <= 31))
+                {
+                    TileScript tile = tiles[(c * 32 + r) - 1].GetComponent<TileScript>();
+                    if (r == row && c == col)
+                    {
+                        tile.RaiseState(TileState.MAX);
+                    }
+                    else
+                    {
+                        float distance = Mathf.Sqrt(  (Mathf.Pow(r - row, 2) + Mathf.Pow(c - col, 2) ) );
+
+                        if (distance >= 2.0f)
+                        {
+                            tile.RaiseState(TileState.QUARTER);
+                        }
+                        else
+                        {
+                            tile.RaiseState(TileState.HALF);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void RevealTile(int row, int col)
+    {
+        if (scansLeft > 0)
+        {
+            for (int c = col - 1; c <= col + 1; c++)
+            {
+                for (int r = row - 1; r <= row + 1; r++)
+                {
+                    if ( (r >= 1 && r <= 32) && (c >= 0 && c <= 31) )
+                    {
+                        TileScript tile = tiles[(c * 32 + r) - 1].GetComponent<TileScript>();
+                        tile.SetVisability(true);
+                    }
+                }
+            }
+
+            scansLeft--;
+            messages.text = "Scans Left: " + scansLeft;
+        }
+        else
+        {
+            messages.text = "You have no more scans left";
+        }
+    }
+
+    public void ExtractTile(int row, int col)
+    {
+        if (extractsLeft > 0)
+        {
+            for (int c = col - 2; c <= col + 2; c++)
+            {
+                for (int r = row - 2; r <= row + 2; r++)
+                {
+                    if ((r >= 1 && r <= 32) && (c >= 0 && c <= 31))
+                    {
+                        TileScript tile = tiles[(c * 32 + r) - 1].GetComponent<TileScript>();
+                        if (row == r && col == c)
+                        {
+                            score += tile.Extract();
+                            scoreText.text = "Score: " + score;
+                            tile.SetVisability(true);
+
+                        }
+                        else
+                        {
+                            tile.Degrade();
+                            if (tile.isVisable)
+                            {
+                                tile.SetVisability(true);
+                            }
+                        }
+                    }
+                }
+            }
+
+            extractsLeft--;
+            if (extractsLeft <= 0)
+            {
+                mode = MinigameMode.Finished;
+                messages.text = "Game Over, you have no more extracts left, please press the start button to play again.";
+            }
+            else
+            {
+                messages.text = "Extracts Left: " + extractsLeft;
+            }
+        }
+    }
+
+    public void ToggleMode()
+    {
+        if (mode.Equals(MinigameMode.Extract))
+        {
+            mode = MinigameMode.Scan;
+            messages.text = "Scans Left: " + scansLeft;
+            modeText.text = "Scan Mode";
+            modeIcon.sprite = scanSprite;
+
+        }
+        else if (mode.Equals(MinigameMode.Scan))
+        {
+            mode = MinigameMode.Extract;
+            messages.text = "Extracts Left: " + extractsLeft;
+            modeText.text = "Extract Mode";
+            modeIcon.sprite = extractSprite;
+        }
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public enum TileState { EMPTY, QUARTER, HALF, MAX };
+public class TileScript : MonoBehaviour, IPointerClickHandler
+{
+    [Header("UI")]
+    [SerializeField]
+    Image icon;
+
+    [Header("Tile Stats")]
+    bool hasBeenModded;
+    public bool isVisable;
+    public int row, col;
+    public MinigameScript miniGame;
+    public TileState state;
+
+    [Header("Tile Class Stats")]
+    [SerializeField]
+    Color emptyColor;
+    [SerializeField]
+    Color quarterColor;
+    [SerializeField]
+    Color halfColor;
+    [SerializeField]
+    Color maxColor;
+    [SerializeField]
+    int emptyValue;
+    [SerializeField]
+    int quarterValue;
+    [SerializeField]
+    int halfValue;
+    [SerializeField]
+    int maxValue;
+    [SerializeField]
+    Sprite diamondSprite;
+    [SerializeField]
+    Sprite questionMarkSprite;
+
+
+
+
+    void Start()
+    {
+        miniGame = transform.parent.GetComponent<MinigameScript>();
+    }
+
+    public void SetGridIndices(int r, int c)
+    {
+        row = r;
+        col = c;
+    }
+
+    public void SetState(TileState newState)
+    {
+        state = newState;
+    }
+
+    public void RaiseState(TileState newState)
+    {
+        if (newState > state)
+        {
+            state = newState;
+        }
+    }
+    public void SetVisability(bool visable)
+    {
+        isVisable = visable;
+
+        if (isVisable)
+        {
+            icon.sprite = diamondSprite;
+            switch (state)
+            {
+                case TileState.EMPTY:
+                    icon.color = emptyColor;
+                    break;
+                case TileState.QUARTER:
+                    icon.color = quarterColor;
+                    break;
+                case TileState.HALF:
+                    icon.color = halfColor;
+                    break;
+                case TileState.MAX:
+                    icon.color = maxColor;
+                    break;
+            }
+        }
+        else
+        {
+            icon.sprite = questionMarkSprite;
+            icon.color = Color.white;
+        }
+
+    }
+
+    public int Extract()
+    {
+        switch (state)
+        {
+            case TileState.EMPTY:
+                return emptyValue;
+            case TileState.QUARTER:
+                SetState(TileState.EMPTY);
+                SetVisability(true);
+                return quarterValue;
+            case TileState.HALF:
+                SetState(TileState.EMPTY);
+                SetVisability(true);
+                return halfValue;
+            case TileState.MAX:
+                SetState(TileState.EMPTY);
+                SetVisability(true);
+                return maxValue;
+            default:
+                return 0;
+        }
+
+    }
+
+    public void Degrade()
+    {
+        switch (state)
+        {
+            case TileState.EMPTY:
+                break;
+            case TileState.QUARTER:
+                SetState(TileState.EMPTY);
+                break;
+            case TileState.HALF:
+                SetState(TileState.QUARTER);
+                break;
+            case TileState.MAX:
+                SetState(TileState.HALF);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        switch (miniGame.mode)
+        {
+            case (MinigameMode.Scan):
+                miniGame.RevealTile(row, col);
+                break;
+
+            case (MinigameMode.Extract):
+                miniGame.ExtractTile(row, col);
+                break;
+        }
+
+    }
+}
